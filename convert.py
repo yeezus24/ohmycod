1e445c79c723d990f6992c132768df8ad3236870
yeezus24 <josephbucket@gmail.com>
2021-02-17 11:01:34 +0000

    fixed so no unecessary output

--- convert.py
+++ convert.py
@@ -2,26 +2,6 @@ import yaml
 import tldextract
 from numpy import array
 
-#def numberify(array_list):
-#	master_domain_list = []
-#	with open('examples.txt', 'r') as examples:
-#		outputs = []
-#		for domains in examples:
-#			domain, value = domains.partition("=")[::2]
-#			domain_list = []
-#			tld = tldextract.extract(domain).suffix
-#			non_tld = tldextract.extract(domain).domain
-#			for i in array_list:
-#				if i in non_tld or i in tld:
-#					domain_list.append(1)
-#				else:
-#					domain_list.append(0)
-#			outputs.append(int(value))
-#			master_domain_list.append(array(domain_list))
-#	return master_domain_list, outputs
-
-
-
 def numberify(url_to_check):
 	examples = open('examples.txt', 'r').read()
 	array_list = open('array_list.txt', 'r').read()
@@ -50,4 +30,3 @@ def numberify(url_to_check):
 				converted_url.append(0)
 
 	return converted_examples, outputs, converted_url
-print(numberify("paypal.op"))
